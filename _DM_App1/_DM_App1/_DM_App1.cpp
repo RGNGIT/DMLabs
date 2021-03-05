d72321de312479d9194fb03533187192eb0b96ea
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-03-06 03:04:02 +0500

    Add files via upload

---
+++ _DM_App1/_DM_App1/_DM_App1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int buff = 0;
+	int Array[8] = { 4, 11, 18, 0, 10, 3, 5, 9 };
+	while (1) {
+		for (int i = 0; i < 8; i++) {
+			if (Array[i] < Array[i + 1]) {
+				buff = Array[i];
+				Array[i] = Array[i + 1];
+				Array[i + 1] = buff;
+			}
+			for (int j = 0; j < 8; j++) {
+				cout << Array[j] << ' ';
+			}
+			cout << '\n';
+		}
+	}
+}
